b7be430eb68179721b619f68ec69983767bdd2b0
Alexander Nissen <alexander@feldbech.dk>
2020-09-22 19:06:30 +0200

    Update home page with project card for easyGraph

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <v-row class="text-center">
-    <v-col> Welcome </v-col>
+  <v-row justify="center">
+    <v-col cols="12" md="8" lg="6">
+      <v-row class="text-center">
+        <v-col>
+          <h1>Alexander Nissen</h1>
+          <h2>Projects</h2>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="4">
+          <ProjectCard title="easyGraph" img="easyGraph.png" link="/easyGraph">
+            Easy to use tool for drawing both directed and undirected graphs
+          </ProjectCard>
+        </v-col>
+      </v-row>
+    </v-col>
   </v-row>
 </template>
 
